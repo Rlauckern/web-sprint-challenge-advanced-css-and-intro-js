ae0f111082886ff12e361b023f02d50beceb4657
Carlton Lauckern <reed@Carltons-MacBook-Air.local>
2021-11-05 08:39:31 -0700

    task-6

--- index.js
+++ index.js
@@ -296,10 +296,17 @@ Use addArtist to do the following:
 
 Example: addArtist(artists) should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/) {
-  /*Your Code Here*/
+function addArtist(array) {
+  array.push({ 
+    id: 20,
+    name: "Reed Lauckern", 
+    years: "1983" - "2021",
+    genre: "Web Design", 
+    nationality: "Caucasian",
+    bio: "Blah de blah. Poof poof whheeee. Bam!",
+  } );
+  return array;
 }
-
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
